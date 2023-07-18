cffde2ca3593bb3ae8f415885b0877efadecbca1
sebin <sebin4548@gmail.com>
2023-07-18 18:40:28 +0900

    stateful

--- lib/main.dart
+++ lib/main.dart
@@ -1,160 +1,56 @@
 import 'package:flutter/material.dart';
-import 'package:toonflix/widgets/button.dart';
-import 'package:toonflix/widgets/currency_card.dart';
 
 void main() {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+  List<int> numbers = [];
+
+  void onClicked() {
+    counter = counter + 1;
+    setState(() {
+      numbers.add(numbers.length);
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        backgroundColor: const Color(0xFF181818),
-        body: SingleChildScrollView(
-          scrollDirection: Axis.vertical,
-          child: Padding(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 20,
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.end,
-                        children: [
-                          Text(
-                            "Hey, Selena",
-                            style: TextStyle(
-                              color: Colors.white,
-                              fontSize: 28,
-                              fontWeight: FontWeight.w800,
-                            ),
-                          ),
-                          Text("Welcome Back",
-                              style: TextStyle(
-                                color: Color.fromRGBO(255, 255, 255, 0.8),
-                                fontSize: 18,
-                              )),
-                        ],
-                      )
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 70,
-                  ),
-                  const Text(
-                    'Total Balance',
-                    style: TextStyle(
-                      color: Color.fromRGBO(255, 255, 255, 0.8),
-                      fontSize: 22,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 5,
-                  ),
-                  const Text(
-                    '\$5 194 482',
-                    style: TextStyle(
-                      fontSize: 48,
-                      fontWeight: FontWeight.w600,
-                      color: Colors.white,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 30,
-                  ),
-                  const Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Button(
-                        text: "transfer",
-                        bgColor: Color(0xFFF1B33B),
-                        textColor: Colors.black,
-                      ),
-                      Button(
-                        text: "request",
-                        bgColor: Color(0xFF1F2123),
-                        textColor: Colors.white,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 100,
-                  ),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        const Text(
-                          'Wallet',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 36,
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                        Text(
-                          'View All',
-                          style: TextStyle(
-                            color: Colors.white.withOpacity(0.8),
-                            // color: Colors.white.withOpacity(0.8),
-                          ),
-                        ),
-                      ]),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const CurrencyCard(
-                    name: "Euro",
-                    code: "EUR",
-                    amount: "22222",
-                    icon: Icons.euro_rounded,
-                    isInverted: false,
-                    num: 0,
-                  ),
-                  const CurrencyCard(
-                    name: "Bitcoin",
-                    code: "btc",
-                    amount: "22222",
-                    icon: Icons.currency_bitcoin,
-                    isInverted: true,
-                    num: 1,
-                  ),
-                  const CurrencyCard(
-                    name: "Euro",
-                    code: "EUR",
-                    amount: "22222",
-                    icon: Icons.euro_rounded,
-                    isInverted: false,
-                    num: 2,
-                  ),
-                ],
-              )),
-        ),
-      ),
-    );
+        home: Scaffold(
+      backgroundColor: const Color(0xFFF4EDDB),
+      body: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            "Click Count",
+            style: TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          for (var n in numbers) Text("$n"),
+          Text(
+            "$counter",
+            style: const TextStyle(
+              fontSize: 30,
+            ),
+          ),
+          IconButton(
+            iconSize: 100,
+            onPressed: onClicked,
+            icon: const Icon(Icons.add_box_outlined),
+          )
+        ],
+      )),
+    ));
   }
 }
-
-// class ActivationFlag {
-//   late bool _flag;
-
-//   ActivationFlag(bool givenFlag) : _flag = givenFlag;
-//   bool get activated => _flag;
-//   set activated(bool givenFlag) => (_flag = givenFlag);
-// }
-// void main(){
-//   try{
-
-//   }
-// }
