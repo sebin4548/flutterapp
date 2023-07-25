cdfcbfae2838fc5153d07df39af3d13ba073bb56
sebin <sebin4548@gmail.com>
2023-07-25 20:49:50 +0900

    sss

--- lib/main.dart
+++ lib/main.dart
@@ -4,84 +4,25 @@ void main() {
   runApp(const App());
 }
 
-class App extends StatefulWidget {
+class App extends StatelessWidget {
   const App({super.key});
-
-  @override
-  State<App> createState() => _AppState();
-}
-
-class _AppState extends State<App> {
-  bool showTitle = true;
-  void toggleTitle() {
-    setState(() {
-      showTitle = !showTitle;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        theme: ThemeData(
-          textTheme: const TextTheme(
-            titleLarge: TextStyle(
-              color: Colors.red,
-            ),
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSwatch(
+          backgroundColor: const Color(0xFFE7626C),
+        ),
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            color: Color(0xFF232B55),
+            // color: Color(0xFF790000),
           ),
         ),
-        home: Scaffold(
-          backgroundColor: const Color(0xFFF4EDDB),
-          body: Center(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              showTitle ? const MyLargeTitle() : const Text("data"),
-              IconButton(
-                onPressed: toggleTitle,
-                icon: const Icon(Icons.remove_red_eye),
-              )
-            ],
-          )),
-        ));
-  }
-}
-
-class MyLargeTitle extends StatefulWidget {
-  const MyLargeTitle({
-    super.key,
-  });
-
-  @override
-  State<MyLargeTitle> createState() => _MyLargeTitleState();
-}
-
-class _MyLargeTitleState extends State<MyLargeTitle> {
-  @override
-  //initState는 모든 상황에서 필요한게 아니다.
-  void initState() {
-    print("init");
-    // TODO: implement initState
-    super.initState();
-  }
-
-  //dispose method는 무언기를 취소하는 메소드
-  @override
-  void dispose() {
-    // TODO: implement dispose
-
-    super.dispose();
-    print("disposed");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Text(
-      'My Large Title',
-      style: TextStyle(
-        fontSize: 30,
-        // 부모 요소인 textTheme을 접근하기 위한 과정
-        color: Theme.of(context).textTheme.titleLarge!.color,
+        cardColor: const Color(0xFFF4EDDB),
       ),
+      home: const App(),
     );
   }
 }
